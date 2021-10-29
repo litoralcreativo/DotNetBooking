b71d22dbe3250272977996c658a6a184b9be8ec2
gaston chatelet <gastonchatelet@gmail.com>
2021-10-29 11:22:55 -0300

    cree clase encargada de importacion y exportacion

---
+++ Booking/ImportacionExportacion.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Booking
+{
+    public static class ImportacionExportacion
+    {
+        public static Empresa ImportarEmpresa(string path)
+        {
+            Empresa nueva = new Empresa();
+            return nueva;
+        }
+        public static bool Exportar(Empresa actual, string path)
+        {
+            bool resultado = false;
+            return resultado;
+        }
+    }
+}
+
+
+
